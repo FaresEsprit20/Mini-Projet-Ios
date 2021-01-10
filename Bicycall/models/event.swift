c62e7bfa5177825f892d9c69e1629a6e4ac6894c
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-01-10 03:27:05 -0800

    llm

--- Bicycall/models/event.swift
+++ Bicycall/models/event.swift
@@ -1,8 +1,8 @@
 //
-//  event.swift
+//  Event.swift
 //  Bicycall
 //
-//  Created by Jamil Joo on 8/1/2021.
+//  Created by Fares Ben Slama on 10/1/2021.
 //
 
 import Foundation
@@ -10,7 +10,7 @@ import Foundation
 class Event: Codable {
     var event_id: Int
     var event_title: String
-    var adress_evt: String
+    var address_evt: String
     var date_evt: String
     var time_evt: String
     var user: Int
@@ -18,11 +18,11 @@ class Event: Codable {
     init(id: Int ,event_title: String ,adress_evt: String ,date_evt: String ,time_evt: String, user: Int ){
         self.event_id = id
         self.event_title = event_title
-        self.adress_evt = adress_evt
+        self.address_evt = adress_evt
         self.date_evt = date_evt
         self.time_evt = time_evt
         self.user = user
-        
-    
     }
+
+    
 }
